dea2e7f51d40de6005ca94c108e849e871d8211d
DMickulesku <danmickulesku@gmail.com>
2017-07-24 15:45:49 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 3000
+
+app.use(function (req, res) {
+  res.send('connected')
+})
+
+app.listen(port, function () {
+  console.log('listening on port', port);
+})
