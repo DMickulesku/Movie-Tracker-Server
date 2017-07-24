171ab29998a5e6a1209e902dbd181a14703855b6
DMickulesku <danmickulesku@gmail.com>
2017-07-24 16:21:07 -0600

    second commit

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 3000
 
+const env = 'development';
+const config = require('./knexfile') [env];
+const knex = require(knex)(config);
+
+
 app.use(function (req, res) {
   res.send('connected')
 })
