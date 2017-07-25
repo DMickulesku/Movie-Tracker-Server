c47bcdb4896333bc84ff782344c4466ca2a34bc9
DMickulesku <danmickulesku@gmail.com>
2017-07-25 14:50:46 -0600

    db/knex.js update

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 3000
 
-const env = 'development';
-const config = require('./knexfile') [env];
-const knex = require(knex)(config);
 
 
-app.use(function (req, res) {
+app.use('/', function (req, res) {
   res.send('connected')
 })
 
