68b4753f2fc66ff24474cbd578b08ddeabc7311b
DMickulesku <danmickulesku@gmail.com>
2017-07-26 17:11:46 -0600

    CRUD commit

--- routes/routes.js
+++ routes/routes.js
@@ -9,4 +9,45 @@ router.get('/', function (req, res) {
     })
 })
 
+router.get('/:id', (req, res) => {
+  let id = req.params.id
+  knex('movies')
+    .where('id', id)
+    .then(movies => {
+      res.json(movies)
+    })
+})
+
+router.post('/', (req, res) => {
+  let post = req.body;
+  knex('movies').insert(post)
+  .returning('*')
+  .then(movies)
+    res.json(movies)
+
+})
+
+router.put('/:id', (req, res) => {
+  let id = req.params.id
+  let edit = req.body
+  knex('movies').where('id', id)
+  .update(edit)
+  .returning('*')
+  .then(edited => {
+    res.json(edited)
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  let id = req.params.id
+  knex('movies')
+    .where('id', id).del()
+    .then(deleted => {
+      res.json({message: 'Movie Deleted!'
+    })
+  })
+
+})
+
+
 module.exports = router
